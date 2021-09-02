4c465c9042029148ef39169446d620496f087372
ASobakin <asobakin@diasoft.ru>
2021-09-02 23:24:53 +0300

    Основные компоненты terraform (HW_9)

---
+++ terraform/storage-bucket.tf
@@ -0,0 +1,9 @@
+
+provider "yandex" {
+  service_account_key_file = var.service_account_key_file
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
+  version                  = 0.35
+}
+
